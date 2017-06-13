587204191b6d4f5f46b6872e7a29b50908148653
Olivier Renault <monoliv@gmail.com>
2017-06-13 13:58:50 +0100

    adding column & column name - working

--- pandas_ex.py
+++ pandas_ex.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python
 #
 import pandas as pd 
+import numpy
+import hashlib
 
 
-source='/home/orenault/Developments/col-encrypt/data/passwd'
-dest='/home/orenault/Developments/col-encrypt/data/pandas'
 
-df=pd.read_csv(source,delimiter=":");#, names=["user","pwd","uid","gid","comment","home","shell"])
 
-print len(df.columns)
+def hash_value(to_hash):
+  hashed = hashlib.sha256(str(to_hash)).hexdigest()
+  return hashed
 
-df1 = df.iloc[:,1-2]
+def main():
+	source='/home/orenault/Developments/col-encrypt/data/passwd'
+	dest='/home/orenault/Developments/col-encrypt/data/pandas'
+	col=0
 
-with open(dest, 'wb') as dest:
-	df1.to_csv(dest)
+	df=pd.read_csv(source,sep=":", header=None);#, names=["user","pwd","uid","gid","comment","home","shell"])
+	cols = df.iloc[:,[0]]
+	df.insert(col + 1, str(col) + '_enc', cols.applymap(hash_value))
+	with open(dest + '1', 'w') as dest1:
+		df.to_csv(dest1, sep=":", header=True, index=False)
 
-#print toto[toto.columns[0]]
+	df.rename(columns={ col : str(col) + '_hash' }, inplace=True)
+	#df.columns = numpy.arange(0,len(df.columns))
+
+
+	#print df.apply(lambda row: hash_value(row[0]))
+
+	with open(dest + '2', 'w') as dest2:
+		cols.applymap(hash_value).to_csv(dest2, sep=":", header=True, index=False)
+		#df.apply(lambda row: hash_value(row[0])).to_csv(dest)
+
+	#print toto[toto.columns[0]]
+
+if __name__ == "__main__":
+  main()
