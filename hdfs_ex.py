679d9f3188ce4c80ee9d0226c1e14ff2ffb0f834
Olivier Renault <monoliv@gmail.com>
2017-07-10 18:18:08 +0100

    example file demonstrating reading / writing to HDFS using hdfscli

--- hdfs_ex.py
+++ hdfs_ex.py
@@ -2,17 +2,25 @@
 # encoding: utf-8
 
 from hdfs import Config
+
 # Requires avro
 from hdfs.ext.dataframe import read_dataframe, write_dataframe
 from hdfs.ext.kerberos import KerberosClient
 import pandas as pd
 
+hdfscliconf = '/home/orenault/Developments/col-encrypt/conf/hdfscli.cfg'
+
 def generate_conf():
   print toto
 
 def main():
-  client = Config(path='/tmp/hdfscli.conf').get_client()
-  print client
+  client = Config(path=hdfscliconf).get_client()
+  with client.read('/user/orenault/passwd') as input:
+    #print input.read()
+    df=pd.read_csv(input, sep=':', header=None)
+    cols = df.iloc[:,0]
+
+    client.write('/user/orenault/data.avro', cols.to_csv(sep=":", header=True, index=False), overwrite=True)
 
 if __name__ == "__main__":
   main()
