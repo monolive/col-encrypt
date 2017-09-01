d12c84a3ea299c4fd535ae9b5cba0a6de6bc95d0
Olivier Renault <olivier.renault@vanquisbank.co.uk>
2017-09-01 15:40:46 +0100

    fix output of encryption on mltiple line + remvoing leading zero

--- encryption.py
+++ encryption.py
@@ -11,12 +11,14 @@ import sys
 
 def parsing_options():
   parser = argparse.ArgumentParser()
-  parser.add_argument('-f', '--file', action='store', dest='file', help='file path', required=True)
+  parser.add_argument('-i', '--input', action='store', dest='input', help='input file path', required=True)
+  parser.add_argument('-o', '--output', action='store', dest='output', help='output file path - default is <input file name>.[encrypted | decrypted]')
   parser.add_argument('-d', '--delimiter', action='store', dest='delimiter', help='field delimiter (default: %(default)s)', default=":" )
   parser.add_argument('-c', '--column', action='append', nargs='+', type=int, dest='column', help='column to encode - can be used multiple times', required=True)
   parser.add_argument('-k', '--key', action='store', dest='RSAkey', help='key to encrypt / decrypt column', required=True)
-  parser.add_argument('-o', '--operation', choices=['encrypt', 'decrypt'], dest='operation', default='encrypt', help='operation: encrypt and hash or decrypt')
+  parser.add_argument('--operation', choices=['encrypt', 'decrypt'], dest='operation', default='encrypt', help='operation: encrypt and hash or decrypt')
   parser.add_argument('--header', action='store', dest='header', type=int, help='header row (int) - do not specify if no header', default='0')
+  parser.add_argument('--overwrite', action='store_true', dest='overwrite', help='overwrite output file', default='False')  
   try:
     results = parser.parse_args()
   except SystemExit as err:
@@ -29,6 +31,9 @@ def parsing_options():
   else:
     results.header = results.header - 1
 
+  if results.output is None:
+    results.output = output_file_name(results.input, results.operation)
+
   # Check arg
   if results.operation == 'encrypt':
     print "\nData will be hashed and encrypted"
@@ -38,6 +43,13 @@ def parsing_options():
     print "======================\n"
   return results
 
+
+def output_file_name(inputFile, operation):
+  if operation == 'decrypt':
+    outputFile = inputFile + ".decrypted"
+  else:
+    outputFile = inputFile + ".encrypted"
+  return(outputFile)
 
 def hash_value(to_hash):
   hashed = []
@@ -72,7 +84,7 @@ def decrypt(to_decrypt, privateRSA):
   decrypted=[]
   for value in to_decrypt:
     try:
-      PlainText = privateRSA.private_decrypt(value.decode('base64'), M2Crypto.RSA.pkcs1_oaep_padding)
+      PlainText = privateRSA.private_decrypt(str(base64.b64decode(value)), M2Crypto.RSA.pkcs1_oaep_padding)
       decrypted.append(PlainText)
     except:
       print "Error: wrong key? wrong column?"
@@ -84,7 +96,7 @@ def encrypt(to_encrypt, publicRSA):
   #for index, value in enumerate(list(to_encrypt)):
   for value in to_encrypt:
     CipherText = publicRSA.public_encrypt(str(value), M2Crypto.RSA.pkcs1_oaep_padding)
-    encrypted.append(CipherText.encode('base64'))
+    encrypted.append(base64.b64encode(CipherText))
   return encrypted
 
 def main():
@@ -92,9 +104,9 @@ def main():
   fext = file_extension(arg.operation)
 
   # Open output file
-  with open(arg.file + fext, 'wb') as fresults:
+  with open(arg.output, 'wb') as fresults:
     # Load file in dataframe
-    df=pd.read_csv(arg.file, sep=arg.delimiter, header=arg.header)
+    df=pd.read_csv(arg.input, sep=arg.delimiter, header=arg.header, dtype=str)
     
     # Flatten the list of columns
     column = list(itertools.chain.from_iterable(arg.column))
@@ -103,18 +115,18 @@ def main():
 
     # Extract columns which need to be hashed / encrypted
     cols = df.iloc[:,column]
-    colName = df[column].columns
-
+    colName = cols.columns
+    
     if arg.operation == 'decrypt':
       # Do not forget the comma behind the privateRSA
       # the correct python grammer for a singleton tuple is (1,) not (1), 
       # which is just an expr wth the value 1.
-      df[colName]=df[column].apply(decrypt, args=(key,), axis=1)
+      df[colName]=df[colName].apply(decrypt, args=(key,), axis=1)
       df.to_csv(fresults, sep=":", header=True, index=False)
     else:
       # Encrypt then hash - as otherwise we encrypt the hash value
       # Call function encrypt w/ RSAkey - Axis=1 for row
-      encrypted = df[column].apply(encrypt, args=(key,))#, axis=1)
+      encrypted = df[colName].apply(encrypt, args=(key,))#, axis=1)
 
       # Rename header to not clash when merging df + encrypted data frame
       new_column=[]
@@ -127,7 +139,7 @@ def main():
       df = pd.concat([df, encrypted], axis=1)
 
       # Generate a hash
-      df[column] = df[column].apply(hash_value).values
+      df[colName] = df[colName].apply(hash_value).values
       
       # Write to file
       df.to_csv(fresults, sep=":", header=True, index=False)
